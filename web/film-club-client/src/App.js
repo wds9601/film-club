9b43995ec5e44b96b9cb20a2a82e3f0c50757bb0
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-12-16 23:04:53 -0800

    added sticky nav bar, addressed image container sizing issue, added menu button to nav and swtitched sidebar to menu button

--- web/film-club-client/src/App.js
+++ web/film-club-client/src/App.js
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
 import { Box, Button, Collapsible, Heading, Image, Grommet, Layer, ResponsiveContext, Text } from 'grommet';
-import { FormClose, Notification, FormSearch, Search } from 'grommet-icons';
+import { FormClose, Menu, Search } from 'grommet-icons';
 // import './App.css';
 
 let movies = [
-  {
-    // poster_url: 'https://place-puppy.com/300x300',
-    title: 'Drive',
-    year: 2009,
-    poster_url: 'https://i.ebayimg.com/images/g/V2gAAOSw7URfUkMH/s-l1600.jpg',
-  },
   {
     title: 'Thor: Ragnorok',
     year: 2016,
@@ -19,6 +13,11 @@ let movies = [
     title: 'Kung Fu Panda',
     year: 2006,
     poster_url: 'https://images-na.ssl-images-amazon.com/images/I/51XhnMdSQdL._AC_SY450_.jpg',
+  },
+  {
+    title: 'Drive',
+    year: 2009,
+    poster_url: 'https://i.ebayimg.com/images/g/V2gAAOSw7URfUkMH/s-l1600.jpg',
   }
 ]  
 
@@ -26,7 +25,7 @@ const theme = {
   global: {
     colors: {
       brand: '#228BE6',
-      bg: '#0d0d0d',
+      bg: '#fcfcfc',
     },
     font: {
       family: 'Ubuntu',
@@ -37,46 +36,59 @@ const theme = {
   }
 }
 
-const AppBar = (props) => (
+const Header = (props) => (
   <Box 
-    tag="header"
+    as="header"
     direction="row"
+    width="100%"
     align="center"
     justify="between"
     background="brand"
     pad={{left: 'medium', right: 'small', vertical: 'medium'}}
-    elevation="medium"
-    style={{ zIndex: '1' }}
+    elevation="small"
+    style={{ 
+      zIndex: '1',
+      position: 'fixed'
+    }}
     {...props}
   />
 )
 
-const Card = (props) => (
+const PosterCard = (props) => (
   <Box
     flex
     direction="column"
     pad="small"
-    margin="small"
+    margin="medium"
     align="center"
     justify="center"
     round="small"
-    width="70%"
-    height={ {min: "25%", max: "40%"} }
+    width="80%"
+    height="auto"
     background="#3b3b3b"
+    elevation="large"
+    onClick={()=>console.log('clicked a card')}
+
   >
     <Box
       height="medium"
       width="medium"
       round="small"
       overflow="hidden"
+      // border={{
+      //   "side": "bottom",
+      //   "style": "solid",
+      //   "size": "medium"
+      // }}
     >
       <Image 
         fill
+        fit="contain"
         alt="movie poster"
-        
         src={props.movie.poster_url}
       ></Image>
     </Box>
+    <hr />
     <Heading
       margin="small"
     >{props.movie.title}</Heading>
@@ -92,15 +104,16 @@ function App() {
       <ResponsiveContext.Consumer>
         { size => (
           <Box background="bg">
-            <AppBar>
-              <Heading level="1" margin='none'>FilmClub <Text size="medium">by Grommet</Text></Heading>
-              <Button icon={<Search />} onClick={() => setShowSidebar(!showSidebar)} />
-            </AppBar>
+            <Header>
+              <Button icon={<Menu />} onClick={() => setShowSidebar(!showSidebar)} />
+              <Heading level="1" margin='none'>FilmClub</Heading>
+              <Button icon={<Search />} />
+            </Header>
 
-            <Box direction="row" flex overflow={{ horizontal: 'hidden' }} >
+            <Box direction="row" flex overflow={{ horizontal: 'hidden' }} pad={{top:"20%"}}>
               <Box flex align="center" justify="center">
                 {movies.map((movie, id) => (
-                  <Card key={id} movie={movie} />
+                  <PosterCard key={id} movie={movie} />
                 ))}
               </Box>
               {(!showSidebar || size !== 'small') ? (
