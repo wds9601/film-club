b2dac0b5b8c37bc03aeb817016091f68ffcb683b
William Downey-Schell <Will@Williams-MacBook-Air.local>
2021-02-16 16:20:25 -0800

    added prettier formatting config file, formatted component files

--- web/film-club-client/src/components/Header.js
+++ web/film-club-client/src/components/Header.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { Anchor, Box, Heading } from 'grommet'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Anchor, Box, Heading } from 'grommet';
 
+const Header = props => (
+	<Box
+		as="header"
+		direction="row"
+		width="100%"
+		align="center"
+		justify="between"
+		background="accent-4"
+		pad={{ horizontal: 'medium', vertical: 'small' }}
+		margin={{ bottom: 'xsmall' }}
+		style={{
+			zIndex: '1',
+			position: 'fixed'
+		}}
+		{...props}
+	>
+		<Link to="/" style={{ textDecoration: 'none' }}>
+			<Anchor path="/" tag="home" color="dark-2">
+				<Heading level="1" margin="none">
+					FilmClub
+				</Heading>
+			</Anchor>
+		</Link>
+	</Box>
+);
 
-const Header = (props) => (
-    <Box 
-        as="header"
-        direction="row"
-        width="100%"
-        align="center"
-        justify="between"
-        background="accent-4"
-        pad={{horizontal: "medium", vertical: 'small'}}
-        margin={{bottom: 'xsmall'}}
-        style={{ 
-            zIndex: '1',
-            position: 'fixed'
-        }}
-        {...props}
-        >
-            <Link to='/' style={{textDecoration: 'none'}}>
-                <Anchor path='/' tag='home' color='dark-2'>
-                    <Heading level="1" margin='none'>FilmClub</Heading>
-                </Anchor>
-            </Link>
-            
-        </Box>
-    )
-
-export default Header
+export default Header;
