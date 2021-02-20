4ae0443de0d493177e84a331826f2846121e9955
William Downey-Schell <Will@Williams-MacBook-Air.local>
2021-02-20 00:29:59 -0800

    trailer video now plays in modal on button click, added backdrop to check out how they look

--- web/film-club-client/src/components/FilmDetailPage.js
+++ web/film-club-client/src/components/FilmDetailPage.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Button, Image, Heading, Text } from 'grommet';
+import { Box, Button, Image, Heading, Layer, Text, Video } from 'grommet';
 import { CirclePlay, PlayFill } from 'grommet-icons';
 
 import Header from './Header';
 
 const FilmDetailPage = (props) => {
-	// State object to hold the film details when the fetch is complete
 	const [movieDetails, setMovieDetails] = useState({});
+	const [showVideo, setShowVideo] = useState(false);
 
 	// React Router URL Parameter object
 	let { id } = useParams();
 
-	
 	// // Use the {id} to fetch details about the corresponding movie
 	// const getMovieDetails = async (id) => {
 	// 	const response = await fetch(`http://localhost:8080/v1/films/${id}`);
@@ -20,7 +19,7 @@ const FilmDetailPage = (props) => {
 	// 	console.log(data);
 	// 	setMovieDetails(data);
 	// };
-	
+
 	// // Fetch all movie details with {id} from params, on component load
 	useEffect(() => {
 		// Use the {id} to fetch details about the corresponding movie
@@ -29,10 +28,10 @@ const FilmDetailPage = (props) => {
 			const data = await response.json();
 			console.log(data);
 			setMovieDetails(data);
-		}
-		getMovieDetails(id)
+		};
+		getMovieDetails(id);
 	}, [id]);
-	
+
 	let {
 		backdropPath,
 		credits,
@@ -53,106 +52,124 @@ const FilmDetailPage = (props) => {
 		let posterUrls = [];
 		let videoUrls = [];
 
-		images.backdrops.forEach(img => {
-			backdropUrls.push(img.filePath)
-		})
+		images.backdrops.forEach((img) => {
+			backdropUrls.push(img.filePath);
+		});
 
-		images.posters.forEach(img => {
-			posterUrls.push(img.file_path)
-		})
+		images.posters.forEach((img) => {
+			posterUrls.push(img.file_path);
+		});
 
-		videos.forEach(video => {
-			videoUrls.push(video.url)
-		})
-	}
+		videos.forEach((video) => {
+			videoUrls.push(video.url);
+		});
 
-	console.log(images.backdrops)
-	console.log(images.posters)
-
-
-	// useEffect(setMovieDetails(movieDetails), []);
+		// console.log(images.backdrops)
+		// console.log(images.posters)
+	};
 
 	if (movieDetails.images) {
-	return (
-		<Box
-			className="page-box"
-			display="flex"
-			direction="column"
-			justify="around"
-			background="dark-2"
-			height={{ min: '100vh' }}
-			overflow={{
-				vertical: 'scroll',
-			}}
-			pad="large"
-		>
-			<Header />
+		return (
 			<Box
-				className="hero-box"
+				className="page-box"
 				display="flex"
-				direction="row"
+				direction="column"
 				justify="around"
-				align="center"
-				margin={{'top': '4em'}}
+				background="dark-2"
+				height={{ min: '100vh' }}
+				overflow={{
+					vertical: 'scroll',
+				}}
+				pad="large"
 			>
-				<Box className="poster-box" 
-					pad="small" 
-					width={{'min': '5em'}}
+				<Header />
+				<Box
+					className="hero-box"
+					display="flex"
+					direction="row"
+					justify="around"
+					align="center"
+					margin={{ top: '4em' }}
 				>
-					<Box >
+					<Box className="poster-box" pad="small" width={{ min: '5em' }}>
+						<Box>
+							<Image
+								src={`http://localhost:8080/v1/films/images/poster${posterPath}?size=large`}
+								alt={`${title} poster image`}
+							/>
+						</Box>
+						<Box
+							className="trailer-box"
+							display="flex"
+							direction="row"
+							justify="around"
+							onClick={ () => setShowVideo(true) }
+							focusIndicator={false}
+						>
+							<Text>Watch Trailer</Text>
+							<CirclePlay size="25em" color="accent-4" />
+						</Box>
+						{showVideo && 
+							<Layer 
+								modal={true} 
+								responsive={true}
+								onEsc={()=> setShowVideo(false)}
+								onClickOutside={()=> setShowVideo(false)}
+								>
+									<iframe width="560" height="315" src={`https://www.youtube.com/embed/${videos[0].key}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+									{/* https://www.themoviedb.org/movie/550-fight-club#play=BdJKm16Co6M */}
+								{/* <Video controls={false} autoplay={false} preLoad>
+									<source key="video" src={`https://www.youtube.com/watch?v=${videos[0].url}&feature=youtu.be`}/>
+									<track
+										key='cc'
+										label='English'
+										kind='subtitles'
+										srcLang='en'
+										src={videos[0].url}
+										default={true}
+									/>
+								</Video> */}
+							</Layer>
+						}
+					</Box>
+					<Box
+						className="info-box"
+						display="flex"
+						direction="column"
+						justify="between"
+						height="16em"
+					>
+						<Box className="title-box">
+							<Heading level="2" margin="0">
+								{title}
+							</Heading>
+							<Text size="0.9em">({releaseDate})</Text>
+							<Text size="0.9em">({genres[0].name})</Text>
+						</Box>
+						<br />
+						<Text>{tagline}</Text>
+						<Text>{overview}</Text>
+					</Box>
+				</Box>
+
+				<Box className="media-box">
+					<Box className="images-box">
 						<Image
-							src={`http://localhost:8080/v1/films/images/poster${posterPath}?size=large`}
+							src={`http://localhost:8080/v1/films/images/poster${backdropPath}?size=large`}
 							alt={`${title} poster image`}
 						/>
 					</Box>
-					<Box className="trailer-box" 
-						display="flex" 
-						direction="row" 
-						justify="around" 
-						onClick={() => console.log("You clicked Play Trailer - this needs a modal")}
-					>
-						<Text>Watch Trailer</Text>
-						<CirclePlay size="25em" color="accent-4" />
-					</Box>
+					<Box className="videos-box"></Box>
 				</Box>
-				<Box className="info-box" display="flex" direction="column" justify="between" height="16em">
-					<Box className="title-box">
-						<Heading level="2" margin="0" >{title}</Heading>
-						<Text size="0.9em">({releaseDate})</Text>
-						<Text size="0.9em">({genres[0].name})</Text>
-					</Box>
-					<br />
-					<Text>{tagline}</Text>
-					<Text>{overview}</Text>
+
+				<Box className="credits-box">
+					<Box className="cast-box"></Box>
+					<Box className="crew-box"></Box>
 				</Box>
 			</Box>
-
-			<Box className="media-box" >
-				<Box className="images-box">
-					<Image
-						src={`http://localhost:8080/v1/films/images/poster${backdropPath}?size=medium`}
-						alt={`${title} poster image`}
-					/>
-				</Box>
-				<Box className="videos-box" >
-
-				</Box>
-			</Box>
-
-			<Box className="credits-box" >
-				<Box className="cast-box">
-
-				</Box>
-				<Box className="crew-box" >
-
-				</Box>
-			</Box>
-		</Box>
-	)
+		);
 	} else {
-		return (
-			<Heading level="2">Loading...</Heading>
-		)
+		return <Heading level="2">Loading...</Heading>;
 	}
 };
 
