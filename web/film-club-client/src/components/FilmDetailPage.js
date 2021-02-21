2a34828832a13c887a43833a2a1618d7583b1c1e
William Downey-Schell <Will@Williams-MacBook-Air.local>
2021-02-20 21:06:13 -0800

    working trailers and conditional renders for basic movie data

--- web/film-club-client/src/components/FilmDetailPage.js
+++ web/film-club-client/src/components/FilmDetailPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Button, Image, Heading, Layer, Text, Video } from 'grommet';
-import { CirclePlay, PlayFill } from 'grommet-icons';
+import { Box, Image, Heading, Layer, Text } from 'grommet';
+import { CirclePlay } from 'grommet-icons';
 
 import Header from './Header';
 
@@ -11,14 +11,6 @@ const FilmDetailPage = (props) => {
 
 	// React Router URL Parameter object
 	let { id } = useParams();
-
-	// // Use the {id} to fetch details about the corresponding movie
-	// const getMovieDetails = async (id) => {
-	// 	const response = await fetch(`http://localhost:8080/v1/films/${id}`);
-	// 	const data = await response.json();
-	// 	console.log(data);
-	// 	setMovieDetails(data);
-	// };
 
 	// // Fetch all movie details with {id} from params, on component load
 	useEffect(() => {
@@ -47,27 +39,6 @@ const FilmDetailPage = (props) => {
 		watchProviders,
 	} = movieDetails;
 
-	const getMediaUrls = (images, videos) => {
-		let backdropUrls = [];
-		let posterUrls = [];
-		let videoUrls = [];
-
-		images.backdrops.forEach((img) => {
-			backdropUrls.push(img.filePath);
-		});
-
-		images.posters.forEach((img) => {
-			posterUrls.push(img.file_path);
-		});
-
-		videos.forEach((video) => {
-			videoUrls.push(video.url);
-		});
-
-		// console.log(images.backdrops)
-		// console.log(images.posters)
-	};
-
 	if (movieDetails.images) {
 		return (
 			<Box
@@ -80,7 +51,7 @@ const FilmDetailPage = (props) => {
 				overflow={{
 					vertical: 'scroll',
 				}}
-				pad="large"
+				pad="medium"
 			>
 				<Header />
 				<Box
@@ -88,10 +59,14 @@ const FilmDetailPage = (props) => {
 					display="flex"
 					direction="row"
 					justify="around"
-					align="center"
+					// align="center"
 					margin={{ top: '4em' }}
 				>
-					<Box className="poster-box" pad="small" width={{ min: '5em' }}>
+					<Box
+						className="poster-box"
+						pad={{ right: 'medium' }}
+						width={{ min: '5em' }}
+					>
 						<Box>
 							<Image
 								src={`http://localhost:8080/v1/films/images/poster${posterPath}?size=large`}
@@ -103,63 +78,105 @@ const FilmDetailPage = (props) => {
 							display="flex"
 							direction="row"
 							justify="around"
-							onClick={ () => setShowVideo(true) }
+							onClick={() => setShowVideo(true)}
 							focusIndicator={false}
+							margin={{ top: 'small' }}
+							pad="small"
+							border={{ size: 'small', color: 'accent-4' }}
+							round="small"
 						>
 							<Text>Watch Trailer</Text>
 							<CirclePlay size="25em" color="accent-4" />
 						</Box>
-						{showVideo && 
-							<Layer 
-								modal={true} 
+						{showVideo && (
+							<Layer
+								modal={true}
 								responsive={true}
-								onEsc={()=> setShowVideo(false)}
-								onClickOutside={()=> setShowVideo(false)}
-								>
-									<iframe width="560" height="315" src={`https://www.youtube.com/embed/${videos[0].key}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-									{/* https://www.themoviedb.org/movie/550-fight-club#play=BdJKm16Co6M */}
-								{/* <Video controls={false} autoplay={false} preLoad>
-									<source key="video" src={`https://www.youtube.com/watch?v=${videos[0].url}&feature=youtu.be`}/>
-									<track
-										key='cc'
-										label='English'
-										kind='subtitles'
-										srcLang='en'
-										src={videos[0].url}
-										default={true}
-									/>
-								</Video> */}
+								onEsc={() => setShowVideo(false)}
+								onClickOutside={() => setShowVideo(false)}
+								round="medium"
+							>
+								{videos[0] ? (
+									<iframe
+										title={title}
+										width="560"
+										height="315"
+										src={`https://www.youtube.com/embed/${videos[0].key}`}
+										frameBorder="0"
+										allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+										allowFullScreen
+									></iframe>
+								) : (
+									<Box
+										display="flex"
+										direction="column"
+										justify="center"
+										align="center"
+										pad="small"
+										responsive={true}
+									>
+										<Text size="1.25em" margin="small" color="light-3">
+											No trailer for this film yet, check back soon!
+										</Text>
+										<Box
+											border={{ size: 'small', color: 'accent-4' }}
+											color="accent-4"
+											alignSelf="center"
+											pad={{ horizontal: 'large' }}
+											align="center"
+											round="xlarge"
+											margin="small"
+											onClick={() => setShowVideo(false)}
+										>
+											close
+										</Box>
+									</Box>
+								)}
 							</Layer>
-						}
+						)}
 					</Box>
+
 					<Box
-						className="info-box"
+						className="info-box-wrapper"
 						display="flex"
 						direction="column"
-						justify="between"
-						height="16em"
+						justify="start"
 					>
-						<Box className="title-box">
-							<Heading level="2" margin="0">
-								{title}
-							</Heading>
-							<Text size="0.9em">({releaseDate})</Text>
-							<Text size="0.9em">({genres[0].name})</Text>
+						<Box
+							className="info-box"
+							display="flex"
+							direction="column"
+							justify="start"
+							height="16em"
+						>
+							<Box className="title-box">
+								<Heading level="1" margin="0">
+									{title}
+								</Heading>
+								<Text size="0.9em">({releaseDate})</Text>
+								{genres[0] && <Text size="0.9em">({genres[0].name})</Text>}
+							</Box>
+							<br />
+							{tagline && (
+								<Text margin={{ top: 'xsmall', bottom: 'medium' }}>
+									{tagline}
+								</Text>
+							)}
+							<Text>{overview}</Text>
 						</Box>
-						<br />
-						<Text>{tagline}</Text>
-						<Text>{overview}</Text>
 					</Box>
 				</Box>
 
 				<Box className="media-box">
-					<Box className="images-box">
-						<Image
-							src={`http://localhost:8080/v1/films/images/poster${backdropPath}?size=large`}
-							alt={`${title} poster image`}
-						/>
-					</Box>
-					<Box className="videos-box"></Box>
+					{images[0] && (
+						<Box className="images-box">
+							<Image
+								src={`http://localhost:8080/v1/films/images/poster${backdropPath}?size=large`}
+								alt={`${title} poster image`}
+							/>
+						</Box>
+					)}
+					{videos[0] && <Box className="videos-box"></Box>}
 				</Box>
 
 				<Box className="credits-box">
