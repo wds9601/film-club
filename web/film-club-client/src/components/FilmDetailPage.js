bc3faf29af9e0c11e4539d84a5972d0c1750dd44
William Downey-Schell <Will@Williams-MacBook-Air.local>
2021-03-08 12:29:20 -0800

    messing with text formatting in poster-info-box

--- web/film-club-client/src/components/FilmDetailPage.js
+++ web/film-club-client/src/components/FilmDetailPage.js
@@ -31,17 +31,16 @@ const FilmDetailPage = () => {
     images,
     genres,
     overview,
-		posterPath,
+    posterPath,
     releaseDate,
     tagline,
-		title,
+    title,
     videos,
   } = movieDetails;
 
-
-  let formattedReleaseDate
+  let formattedReleaseDate;
   if (releaseDate) {
-    formattedReleaseDate = new Date(releaseDate)
+    formattedReleaseDate = new Date(releaseDate);
     // Format release date display to "MM.DD.YY"
     const day = formattedReleaseDate.getDate();
     const month = formattedReleaseDate.getMonth() + 1;
@@ -54,121 +53,133 @@ const FilmDetailPage = () => {
     formattedReleaseDate = `${month}.${day}.${year}`;
   }
 
-
   if (images) {
+    // Check if movieDetail object has poster image:
+    // if it does - use the poster image path for fetch
+    // If it doesnt - use the default poster image
+    let posterUrl = posterPath
+      ? `/films/images/poster${posterPath}?size=medium`
+      : defaultMoviePoster;
 
-		// Check if movieDetail object has poster image:
-		// if it does - use the poster image path for fetch
-		// If it doesnt - use the default poster image
-		let posterUrl = posterPath
-			? `/films/images/poster${posterPath}?size=medium`
-			: defaultMoviePoster;
-		
-		//  If the movieDetails payload has a videos object, filter for the official trailer
-		let movieTrailer = videos.filter(video => video.type === 'Trailer')[0];
+    //  If the movieDetails payload has a videos object, filter for the official trailer
+    let movieTrailer = videos.filter((video) => video.type === 'Trailer')[0];
 
     return (
       <Box
         className="page-box"
-        display="flex"
         direction="column"
         justify="around"
         background="dark-2"
         overflow={{
           vertical: 'scroll',
         }}
-        pad="medium"
+        pad={{vertical: "4em", horizontal: "large"}}
       >
         <Header />
 
-      <Box className="poster-info-box"
-        display="flex"
-        direction="column"
-        justify="around"
-        align="center"
-        pad="medium"
-        border={{color: 'accent-4', size: 'small'}}
-      >
-        <Box
-          className="poster-box"
-          margin={{bottom: '1em'}}
-        >
-          <Box>
-            <Image
-              src={posterUrl}
-              alt={`${title} poster image`}
-            />
-          </Box>
-        </Box>
-
-        <Box className="info-box">
-          <Box className="text-box" text-align='start'>
-            <Heading level="2" margin="0">
-              {title}
-            </Heading>
-            <Box className="text-header-group">
-              <Text size="0.9em" weight="bold">Release Date:</Text><Text size="0.9em">{formattedReleaseDate}</Text>
-            </Box>
-            <Box className="text-header-group">
-              <Text size="0.9em" weight="bold">Genres:</Text>
-              {genres[0] && (
-                genres.map(genre => (
-                  <Text size="0.9em">
-                    {genre.name}
-                  </Text>
-                )))
-              }
-            </Box>
-            
-          </Box>
-          <br />
-          <Box 
-            className="info-text-box" 
-            display="flex" 
-            direction="column" 
-          >
-            {tagline && (
-              <Box className="text-header-group">
-                <Text size="0.9em" weight="bold">Tagline:</Text>
-                <Paragraph margin={{ top: 'xsmall', bottom: 'medium' }}>
-                  {tagline}
-                </Paragraph>
-              </Box>
-            )}
-            {overview && (
-              <Box className="text-header-group">
-                <Text size="0.9em" weight="bold">Overview:</Text>
-                <Paragraph margin={{ top: 'xsmall'}}>
-                  {overview}
-                </Paragraph>
-              </Box>
-            )}
-          </Box>
-        </Box>
-      </Box>
-
-        <Box
-          className="hero-box"
-          display="flex"
-          direction="row"
-          justify="around"
-        >
-        
-
+        {videos[0] ? (
           <Box
-            className="info-box-wrapper"
-            display="flex"
-            direction="column"
-            justify="start"
+            width="100%"
+            round="small"
+            border={{ color: 'accent-4', size: 'small' }}
+            overflow="hidden"
           >
+            <iframe
+              title={movieTrailer.name}
+              width="100%"
+              height="300"
+              src={`https://www.youtube.com/embed/${movieTrailer.key}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </Box>
+        ) : (
+          <Box
+            direction="column"
+            justify="center"
+            align="center"
+            pad="small"
+            margin={{ top: 'xlarge' }}
+            round="small"
+            border={{ color: 'accent-4', size: 'small' }}
+            responsive={true}
+          >
+            <Text size="1.25em" margin="small" color="light-3">
+              No trailer for this film yet, check back soon!
+            </Text>
+          </Box>
+        )}
+
+        <Box
+          className="poster-info-box"
+          direction="column"
+          justify="around"
+          align="center"
+          pad="medium"
+          round="small"
+          border={{ color: 'accent-4', size: 'small' }}
+          margin={{ top: 'large' }}
+        >
+          <Box className="poster-box" margin={{ bottom: '1em' }}>
+            <Box>
+              <Image src={posterUrl} alt={`${title} poster image`} />
+            </Box>
+          </Box>
+
+          <Box className="info-box">
+            <Box className="all-text-box" text-align="start">
+
+              <Text size="2.5em" weight="bold">
+                {title}
+              </Text>
+
+              <Box className="sub-text-box" pad="small">
+                <Box className="text-header-group" direction="row" align="center">
+                  <Text weight="bold" margin={{right: "small"}}>
+                    Release Date:
+                  </Text>
+                  <Text size="0.9em">{formattedReleaseDate}</Text>
+                </Box>
+                <Box className="text-header-group" direction="row" align="start">
+                  <Text weight="bold" margin={{right: "small"}}>
+                    Genres:
+                  </Text>
+                  <Box direction="column" >
+                    {genres[0] &&
+                      genres.map((genre) =>
+                        <Text size="0.9em">{genre.name}</Text>
+                    )}
+                  </Box>
+                </Box>
+                <br />
+                <Box className="info-text-box" direction="column">
+                  {tagline && (
+                    <Box className="text-header-group">
+                      <Text size="0.9em" margin={{ top: 'xsmall', bottom: 'medium' }}>
+                        <em>{tagline}</em>
+                      </Text>
+                    </Box>
+                  )}
+                  {overview && (
+                    <Box className="text-header-group">
+                      <Text size="0.9em" margin={{ top: 'xsmall' }}>{overview}</Text>
+                    </Box>
+                  )}
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box className="hero-box" direction="row" justify="around">
+          <Box className="info-box-wrapper" direction="column" justify="start">
             <Box
               className="info-box"
-              display="flex"
               direction="column"
               justify="start"
               height="16em"
-            >
-            </Box>
+            ></Box>
           </Box>
         </Box>
 
@@ -191,68 +202,3 @@ const FilmDetailPage = () => {
 };
 
 export default FilmDetailPage;
-
-
-// Watch trailer button, modal trailer video player
-// Should be only trailer video player on page load, no button
-{/* <Box
-className="trailer-box"
-display="flex"
-direction="row"
-justify="around"
-onClick={() => setShowVideo(true)}
-focusIndicator={false}
-margin={{ top: 'small' }}
-pad="small"
-border={{ size: 'small', color: 'accent-4' }}
-round="small"
->
-<Text>Watch Trailer</Text>
-<CirclePlay size="25em" color="accent-4" />
-</Box>
-{showVideo && (
-<Layer
-  modal={true}
-  responsive={true}
-  onEsc={() => setShowVideo(false)}
-  onClickOutside={() => setShowVideo(false)}
-  round="medium"
->
-  {videos[0]  ? (
-    <iframe
-      title={movieTrailer.name}
-      width="560"
-      height="315"
-      src={`https://www.youtube.com/embed/${movieTrailer.key}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    ></iframe>
-  ) : (
-    <Box
-      display="flex"
-      direction="column"
-      justify="center"
-      align="center"
-      pad="small"
-      responsive={true}
-    >
-      <Text size="1.25em" margin="small" color="light-3">
-        No trailer for this film yet, check back soon!
-      </Text>
-      <Box
-        border={{ size: 'small', color: 'accent-4' }}
-        color="accent-4"
-        alignSelf="center"
-        pad={{ horizontal: 'large' }}
-        align="center"
-        round="xlarge"
-        margin="small"
-        onClick={() => setShowVideo(false)}
-      >
-        close
-      </Box>
-    </Box>
-  )}
-</Layer>
-)} */}
